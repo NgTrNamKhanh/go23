974cbb190adcdae3a75eb2f4c85f1f0a8bad0ebf
NgTrNamKhanh <109201894+NgTrNamKhanh@users.noreply.github.com>
2023-07-24 10:42:12 +0700

    fix

--- ex01/ex01.go
+++ ex01/ex01.go
@@ -3,9 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"go23/ex01/ulti"
 	"os"
-	"strings"
-	"github.com/NgTrNamKhanh/go23/ex01/ulti/ulti"
 )
 
 func main() {
@@ -15,47 +14,6 @@ func main() {
 
 	input, err := reader.ReadString('\n')
 
-
-	result := 
-	fmt.Print()
-
-	if err != nil {
-		fmt.Println("Error reading input:", err)
-		return
-	}
-
-	parts := strings.Split(input, " ")
-
-	firstName := parts[0]
-	var lastName string
-	var middleName string
-
-	var countryCode string
-	if len(parts) >= 3 {
-		countryCode = strings.TrimSpace(parts[len(parts)-1])  // Trim the newline character
-		middleName = strings.Join(parts[1:len(parts)-2], " ") // Fix this line
-		lastName = parts[len(parts)-2]
-	} else if len(parts) == 2 {
-		lastName = parts[1]
-		countryCode = strings.TrimSpace(parts[0]) // Trim the newline character
-	} else {
-		fmt.Println("Error")
-		return
-	}
-
-	var fullName string
-	fmt.Println(countryCode)
-	switch countryCode {
-	case "VN", "CN", "JP", "KR", "IN", "TH", "ID", "PH", "MY", "SG":
-		fullName = lastName + " " + middleName + " " + firstName
-	case "US", "CA", "GB", "FR", "DE", "IT", "ES", "AU", "NZ", "BR":
-		fullName = firstName + " " + middleName + " " + lastName
-	default:
-		fmt.Println("Invalid country code")
-		return
-	}
-
-	fmt.Println(fullName)
+	result := ulti.Convert(input,err);
+	fmt.Print(result)
 }
-
-
--- ex01/ulti/ulti.go
+++ ex01/ulti/ulti.go
@@ -1,1 +1,52 @@
 package ulti
+
+import (
+	"errors"
+	"strings"
+)
+
+func Convert(input string, err error) string {
+	if err != nil {
+		return err.Error()
+	}
+	parts := strings.Split(input, " ")
+	handleMiddleName(parts)
+	handleMiddleName(parts)
+	countryCode, firstName, middleName, lastName, err := handleMiddleName(parts)
+	if err != nil {
+		return err.Error()
+	}
+	fullName := handleCountryCode(countryCode, firstName, middleName, lastName)
+	return fullName
+}
+
+func handleMiddleName(parts []string) (string, string, string, string, error) {
+	firstName := parts[0]
+	var lastName string
+	var middleName string
+	var countryCode string
+	if len(parts) >= 3 {
+		countryCode = strings.TrimSpace(parts[len(parts)-1])
+		middleName = strings.Join(parts[1:len(parts)-2], " ")
+		lastName = parts[len(parts)-2]
+	} else if len(parts) == 2 {
+		lastName = parts[1]
+		countryCode = strings.TrimSpace(parts[0])
+	} else {
+		return "", "", "", "", errors.New("not enough parts to form a name")
+	}
+	return countryCode, firstName, middleName, lastName, nil
+
+}
+func handleCountryCode(countryCode, firstName, middleName, lastName string) string {
+	var fullName string
+	switch countryCode {
+	case "VN", "CN", "JP", "KR", "IN", "TH", "ID", "PH", "MY", "SG":
+		fullName = lastName + " " + middleName + " " + firstName
+	case "US", "CA", "GB", "FR", "DE", "IT", "ES", "AU", "NZ", "BR":
+		fullName = firstName + " " + middleName + " " + lastName
+	default:
+		return "Invalid contry code"
+	}
+	return fullName
+}
